1377e447cc0b9503574d8bbb3309f5384d025fad
Andrew Boessen <118072740+AndrewBoessen@users.noreply.github.com>
2024-12-21 13:43:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def profile_readme():
 I'm a Computer Science student at Boston College, with a minor in Finance. I'm deeply interested in software engineering, machine learning, and building innovative projects that solve real-world problems.
 
 ### Projects
-1. [Perfect Rep](https://github.com/AndrewBoessen/PerfectRep) - 3D computer vision model for powerlifting analysis using PyTorch and Transformers
-2. [Eagle Eval](https://github.com/AndrewBoessen/EagleEval) - Platform for Boston College course ratings using TypeScript, Angular, Express, and MongoDB
-3. [Re-Clip](https://github.com/AndrewBoessen/Re-Clip) - AI-powered mobile app for generating short-form video content from research papers and GitHub repos
-4. [CUDAgrad](https://github.com/AndrewBoessen/CUDAgrad) - GPU-accelerated autograd engine and neural network library using CUDA and C
-5. [1D Tokenizer](https://github.com/AndrewBoessen/simple-1d-tokenizer) - A neural architecture for encoding images into sequences of discrete tokens, enabling efficient image compression
+1. [Neural Game Engine](https://github.com/AndrewBoessen/neural-game-engine) - Neural network approach for modeling interactive game environments using a VQ-VAE and Spatio-Temporal Transformers.
+2. [Perfect Rep](https://github.com/AndrewBoessen/PerfectRep) - 3D computer vision model for powerlifting analysis using PyTorch and Transformers
+3. [Eagle Eval](https://github.com/AndrewBoessen/EagleEval) - Platform for Boston College course ratings using TypeScript, Angular, Express, and MongoDB
+4. [Re-Clip](https://github.com/AndrewBoessen/Re-Clip) - AI-powered mobile app for generating short-form video content from research papers and GitHub repos
+5. [CUDAgrad](https://github.com/AndrewBoessen/CUDAgrad) - GPU-accelerated autograd engine and neural network library using CUDA and C
+6. [1D Tokenizer](https://github.com/AndrewBoessen/simple-1d-tokenizer) - A neural architecture for encoding images into sequences of discrete tokens, enabling efficient image compression
 '''
 
     return readme_content
